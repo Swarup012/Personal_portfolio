980bf25df80e699282189a48753bf7c04d386121
Swarup <swarupbasu325@gmail.com>
2024-06-26 16:09:13 +0530

    portfolio

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -3,18 +3,18 @@ export const educations = [
     id: 1,
     title: "Bachelor Degree",
     duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    institution: "Eminent College of Management & Technology",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
     duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    institution: "Nibadhai High School",
   },
   {
     id: 3,
     title: "Secondary School Certificate",
     duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    institution: "Nibadhai High School",
   }
 ]
