bfb0785029414d32a199f7b9b28aa80ffc43e935
Swarup <96013874+Swarup012@users.noreply.github.com>
2026-02-03 17:28:02 +0530

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -2,19 +2,14 @@ export const experiences = [
   {
     id: 1,
     title: 'Backend Developer',
-    company: "editxpert.com",
-    duration: "(Jan 2022 - November 2023)"
+    company: "Freelance",
+    duration: "(Jan 2024 - November 2025)"
   },
   {
     id: 2,
     title: "FullStack Developer",
-    company: "Freelance (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    company: "Temcia Digital",
+    duration: "(Jan 2025 - Nov 2025)"
   },
-  {
-    id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2022 - Present)"
-  }
+
 ]
