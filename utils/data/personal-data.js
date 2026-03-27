45dfcd4f1dbb57dc13ca20a118ff37cf9e561eda
Swarup <96013874+Swarup012@users.noreply.github.com>
2026-03-27 11:55:32 +0530

    Change designation to 'Full-Stack Developer' and update resume link
    Updated the designation and resume link in personal data.

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "Swarup Basu",
   profile: '/profile.jpeg',
-  designation: "Software Developer",
+  designation: "Full-Stack Developer",
   description: "My name is Swarup Basu. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and also Java  .I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'swarupbasu325@gmail.com',
   phone: '+918240635683',
@@ -13,5 +13,5 @@ export const personalData = {
   stackOverflow: 'https://stackoverflow.com',
   leetcode: "https://leetcode.com/u/swarupbasu325/",
   devUsername: "swarup",
-  resume: "https://drive.google.com/file/d/1OuWq0-u0u3HUrieHXI6WQOWh_c0ROqYm/view"
+  resume: "https://drive.google.com/file/d/1XE4f2q3dBxoSUWLDZxdt6aGV1-T5ziV6/view?usp=sharing"
 }
